69267af3a9b48a914a6453433c44703b5f4d05a1
Amrutha M <amrutha.m273@gmail.com>
2026-06-26 23:05:43 +0530

    Initial commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,28 @@
+import "./Hero.css";
+
+function Hero(){
+    return(
+        <section id="home" className="hero">
+            <div className="hero-content">
+                  <h3>Hello, I'm</h3>
+
+                    <h1>Amrutha M</h1>
+
+                    <h2>Software Developer</h2>
+
+                    <p>
+                    Passionate about Backend Development, AI/ML and building full stack
+                    applications.
+                    </p>
+
+                       <div className="hero-buttons">
+                            <button>Download Resume</button>
+                       </div>
+            </div>
+            <div className="hero-image">
+
+            </div>
+        </section>
+    );
+}
+export default Hero;
